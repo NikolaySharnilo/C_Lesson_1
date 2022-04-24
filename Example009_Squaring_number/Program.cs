1363fcd6beefc913b90929ecdcaf1113885164da
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-04-24 17:39:39 +0500

    Delete comment

--- Example009_Squaring_number/Program.cs
+++ Example009_Squaring_number/Program.cs
@@ -1,11 +1,11 @@
 ﻿//ПРограмма для возмедения в квадрат
-/*
+
 Console.Write("Введите целое число: ");
 int number = int.Parse(Console.ReadLine());
 
 int result = number * number;
 
 Console.WriteLine(result);
-*/
+
 
 
