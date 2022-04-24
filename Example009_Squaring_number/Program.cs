eb8c09cf9bee1da875ba43e7ff7140a71a024297
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-04-24 18:09:16 +0500

    Add code If-Else First number squre second number

--- Example009_Squaring_number/Program.cs
+++ Example009_Squaring_number/Program.cs
@@ -1,11 +1,25 @@
 ﻿//ПРограмма для возмедения в квадрат
-
+/*
 Console.Write("Введите целое число: ");
 int number = int.Parse(Console.ReadLine());
 
 int result = number * number;
 
 Console.WriteLine(result);
+*/
 
+//Программа, которая принимает два числа и проверяет, является ли первое число квадратом второго
 
+Console.Write("Введите первое целое число: ");
+int numberA = int.Parse(Console.ReadLine());
 
+Console.Write("Введите второе целое число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if ((numberA*numberA)==numberB) {
+    Console.WriteLine("Первое число " + numberA + " является квадратом числа "+ numberB);
+}
+else
+{
+    Console.WriteLine("Первое число не является квадртом второго");
+}
