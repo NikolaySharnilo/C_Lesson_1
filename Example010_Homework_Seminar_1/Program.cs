b019b4a5a88961f61978ca7951a433d96da036c6
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-04-24 19:17:39 +0500

    Add Task 1. Number Module

--- Example010_Homework_Seminar_1/Program.cs
+++ Example010_Homework_Seminar_1/Program.cs
@@ -1,2 +1,12 @@
-﻿
+﻿//Задача 1: Напишите программу вычисления модуля числа
+
+
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 0)
+{
+    number = -number;
+}
+Console.WriteLine("Введенное число по модулю: " + number);
+
 
