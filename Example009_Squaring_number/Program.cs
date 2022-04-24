9905189353bc571a459c311a532c6414ac58d73f
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-04-24 18:20:35 +0500

    Add day of week

--- Example009_Squaring_number/Program.cs
+++ Example009_Squaring_number/Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine(result);
 */
 
 //Программа, которая принимает два числа и проверяет, является ли первое число квадратом второго
-
+/*
 Console.Write("Введите первое целое число: ");
 int numberA = int.Parse(Console.ReadLine());
 
@@ -22,4 +22,43 @@ if ((numberA*numberA)==numberB) {
 else
 {
     Console.WriteLine("Первое число не является квадртом второго");
+}
+*/
+
+//Программа для вывода названия дня недели по введенному номеру
+
+Console.Write("Введите число для определения дня недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number==1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (number==2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (number==3)
+{
+    Console.WriteLine("Среда");
+}
+else if (number==4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (number==5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (number==6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (number==7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else 
+{
+    Console.WriteLine("Введите корректное число");
 }
