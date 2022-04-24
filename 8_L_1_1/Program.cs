32abbacb59aa02bec52852b8c9ae778618e2d9a1
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-04-24 17:32:17 +0500

    Add code squaring number

---
+++ 8_L_1_1/Program.cs
@@ -0,0 +1,7 @@
+﻿//ПРограмма для возмедения в квадрат
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = number * number;
+
+Console.WriteLine(result);
