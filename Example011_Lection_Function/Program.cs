6634607596eb04d56eed430da774048e72196eed
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-05-18 06:24:54 +0500

    New numbers

--- Example011_Lection_Function/Program.cs
+++ Example011_Lection_Function/Program.cs
@@ -1,4 +1,4 @@
-﻿int Max(int arg1, int arg2, arg3)
+﻿int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
     if (arg2>result) result = arg2;
@@ -6,3 +6,28 @@
     return result;
 }
 
+int a1 = 23;
+int b1 = 22;
+int c1 = 19;
+
+int a2 = 25;
+int b2 = 9434;
+int c2 = 84;
+
+int a3 = 26;
+int b3 = 522;
+int c3 = 84;
+
+
+/*int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);*/
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3)
+);
+
+Console.WriteLine(max);
