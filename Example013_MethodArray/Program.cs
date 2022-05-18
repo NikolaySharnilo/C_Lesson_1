eae23df615600b7b913519107f287d213f010ed3
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-05-18 06:45:09 +0500

    Add MethodArray

---
+++ Example013_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 22, 33, 4, 18, 66, 77, 53, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+            Console.WriteLine(index);
+            break;
+    }
+
+    index++;
+}
