765a768eb77de5f1d1d809bf190f451d16f4f3d6
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-05-19 20:00:27 +0500

    Add LastNumber

--- Example015_SeminarFunction/Program.cs
+++ Example015_SeminarFunction/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int NumberRead ()
+{
+    Console.Write("Введите число: ");
+    string a = Console.ReadLine();
+    return int.Parse(a);
+}
+
+int LastNumber(int number)
+{
+     number = number % 10;
+     return number;
+}
+
+Console.WriteLine (LastNumber(NumberRead()));
