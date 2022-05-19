4e32090e99f1b21c44219c834e9d884d13c5dd42
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-05-19 20:28:24 +0500

    Add Find Divisible number

--- Example015_SeminarFunction/Program.cs
+++ Example015_SeminarFunction/Program.cs
@@ -1,10 +1,10 @@
-﻿/*int NumberRead ()
+﻿int NumberRead ()
 {
     Console.Write("Введите число: ");
     string a = Console.ReadLine();
     return int.Parse(a);
 }
-*/
+
 int LastNumber(int number)
 {
     number = number % 10;
@@ -21,7 +21,7 @@ int FirstNumber(int number)
 int RandomNumber(int number1, int number2)
 {
     int a = new Random().Next(number1, number2);
-    Console.WriteLine (a);
+    Console.WriteLine(a);
     return a;
 }
 
@@ -29,17 +29,28 @@ void MaxNumber(int firstNumber, int lastNumber)
 {
     if (lastNumber < firstNumber)
     {
-         Console.WriteLine("Больше первое число: " + firstNumber);
+        Console.WriteLine("Больше первое число: " + firstNumber);
     }
     else
     {
-         Console.WriteLine("Больше второе число: " + lastNumber);
+        Console.WriteLine("Больше второе число: " + lastNumber);
     }
 }
 
-int a = RandomNumber(10, 99);
-MaxNumber(FirstNumber(a), LastNumber(a));
+//int a = RandomNumber(10, 100);
+//MaxNumber(FirstNumber(a), LastNumber(a));
 
+void FindDivisible (int number1, int number2)
+{
+    if (number1 % number2 == 0)
+    {
+        Console.WriteLine("Числа кратные");
+    }
+    else
+    {
+        Console.WriteLine("Числа не кратные, остаток от деления: " + number1 % number2);
+    }
+}
 
-
+FindDivisible(NumberRead(), NumberRead());
 
