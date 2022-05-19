54717ca95cb23f3d3ee8b2328db4950f6b06c0cf
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-05-19 20:18:28 +0500

    Add Random and find Max Number

--- Example015_SeminarFunction/Program.cs
+++ Example015_SeminarFunction/Program.cs
@@ -1,14 +1,45 @@
-﻿int NumberRead ()
+﻿/*int NumberRead ()
 {
     Console.Write("Введите число: ");
     string a = Console.ReadLine();
     return int.Parse(a);
 }
-
+*/
 int LastNumber(int number)
 {
-     number = number % 10;
-     return number;
+    number = number % 10;
+    return number;
+}
+int FirstNumber(int number)
+{
+    number = number / 10;
+    return number;
 }
 
-Console.WriteLine (LastNumber(NumberRead()));
+//Console.WriteLine (LastNumber(NumberRead()));
+
+int RandomNumber(int number1, int number2)
+{
+    int a = new Random().Next(number1, number2);
+    Console.WriteLine (a);
+    return a;
+}
+
+void MaxNumber(int firstNumber, int lastNumber)
+{
+    if (lastNumber < firstNumber)
+    {
+         Console.WriteLine("Больше первое число: " + firstNumber);
+    }
+    else
+    {
+         Console.WriteLine("Больше второе число: " + lastNumber);
+    }
+}
+
+int a = RandomNumber(10, 99);
+MaxNumber(FirstNumber(a), LastNumber(a));
+
+
+
+
