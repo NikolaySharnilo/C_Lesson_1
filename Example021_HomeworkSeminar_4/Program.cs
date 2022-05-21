188128c0375ff4448a0c80f8af8997daa157882a
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-05-21 19:00:01 +0500

    Task 1: add Function Pow number

--- Example021_HomeworkSeminar_4/Program.cs
+++ Example021_HomeworkSeminar_4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 1: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+
+void PowNumber(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result *= a;
+    }
+    Console.WriteLine(result);
+}
+PowNumber(3, 5);
+PowNumber(2, 4);
