7d80a722008be24f9da261db334d33e44fcd9375
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-05-21 19:28:15 +0500

    Task 3: Add Functions Creat new array and Print Array

--- Example021_HomeworkSeminar_4/Program.cs
+++ Example021_HomeworkSeminar_4/Program.cs
@@ -13,6 +13,7 @@ void PowNumber(int a, int b)
 /* PowNumber(3, 5);
 PowNumber(2, 4); */
 
+//Task 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 void SumNumbers(int num)
 {
     int result = 0;
@@ -23,6 +24,32 @@ void SumNumbers(int num)
     }
     Console.WriteLine(result);
 }
-SumNumbers(452);
+/* SumNumbers(452);
 SumNumbers(82);
-SumNumbers(9012);
+SumNumbers(9012); */
+
+//Task 3: Напишите программу, которая задаёт массив из 8 элементов случайными числами и 
+//выводит их на экран. Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+
+int[] NewArrayRandom(int count)
+{
+    int min = 1;
+    int max = 100;
+    int[] arr = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        arr[i] = new Random().Next(min, max);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(NewArrayRandom(8));
