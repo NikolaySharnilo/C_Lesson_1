0dd2650c58437a47c2a2a77d049101ac90f6d24d
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-06-22 14:13:47 +0500

    Task 0: Copy [,] array

--- Example026_HomeworkSeminar_6/Program.cs
+++ Example026_HomeworkSeminar_6/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] NewArrayRandom(int countR, int countC, int min, int max)
+{
+    int[,] arr = new int[countR, countC];
+    for (int i = 0; i < countR; i++)
+    {
+        for (int j = 0; j < countC; j++)
+        {
+           arr[i,j] = new Random().Next(min, max); 
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Task 0: заполнение двумерного массива и его копирование
+void copyArray()
+{
+    int[,] array = NewArrayRandom(5, 3, 1, 21);
+    PrintArray(array);
+
+    int sizeR = array.GetLength(0);
+    int sizeC = array.GetLength(1);
+    int[,] newArray = new int[sizeR, sizeC];
+
+    for (int i = 0; i < sizeR; i++)
+    {
+        for (int j=0; j< sizeC; j++)
+        {
+            newArray[i,j] = array[i,j];
+        }
+        
+    }
+    PrintArray(newArray);
+}
+copyArray();
