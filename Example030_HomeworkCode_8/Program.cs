82167e2f13808a489643866627d9beef0b90828c
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-06-24 08:05:44 +0500

    git init

---
+++ Example030_HomeworkCode_8/Program.cs
@@ -0,0 +1,26 @@
+﻿int[,] NewArrayRandom(int countR, int countC, int min, int max)
+{
+    int[,] arr = new int[countR, countC];
+    for (int i = 0; i < countR; i++)
+    {
+        for (int j = 0; j < countC; j++)
+        {
+            arr[i, j] = new Random().Next(min, max);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
