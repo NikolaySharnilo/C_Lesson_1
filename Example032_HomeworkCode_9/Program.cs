7a2a9f2edce18231d89504a97c0e6d884f928822
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-06-25 09:47:33 +0500

    Task 1: output eve number

--- Example032_HomeworkCode_9/Program.cs
+++ Example032_HomeworkCode_9/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Task 1: Задайте значения M и N. Напишите программу, которая выведет все четные натуральные числа
+// в промежутке от M до N с помощью рекурсии.
+void ValueSet(int m, int n)
+{
+    if (m > n) return;
+    ValueSet(m, n - 1);
+    if (n % 2 == 0) Console.Write($"{n}, ");
+}
+
+ValueSet(4,8);
