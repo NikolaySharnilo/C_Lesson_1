d6e4b06fae19be5d541590f11012d1373ae77188
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-06-25 09:51:40 +0500

    Task 2: sum number m to n

--- Example032_HomeworkCode_9/Program.cs
+++ Example032_HomeworkCode_9/Program.cs
@@ -7,5 +7,14 @@ void ValueSet(int m, int n)
     ValueSet(m, n - 1);
     if (n % 2 == 0) Console.Write($"{n}, ");
 }
+//ValueSet(4,8);
 
-ValueSet(4,8);
+// Task 2: Задайте значения M и N. Напишите программу, которая 
+//найдет сумму натуральных элементов в промежутке от M до N с помощью рекурсии
+
+int sumNumber(int m, int n)
+{
+    if (m > n) return 0;
+    return sumNumber(m, n - 1) + n;
+}
+Console.WriteLine(sumNumber(4, 8));
