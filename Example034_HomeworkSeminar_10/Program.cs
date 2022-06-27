8e9da9ee518182686e1da74ce48f3fd31c80b4bb
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-06-27 21:03:20 +0500

    Task 1: Recursion number from 1 to N

--- Example034_HomeworkSeminar_10/Program.cs
+++ Example034_HomeworkSeminar_10/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Task 1: Есть монотонная последовательность, каждое число встречается ровно то количество раз, какое значение этого числа
+//: 1,2,2,3,3,3,4,4,4,4... Выведите ряд последовательности до N-го значения, желательно использовать рекурсию
+void RecursionNumOfOneToN(int count, int num = 1, int i = 0)
+{
+
+    if (count == 0) return;
+    Console.Write($"{num}, ");
+
+    if (i == 0)
+    {
+        num++;
+        i = num;
+    }
+    RecursionNumOfOneToN(count - 1, num, i - 1);
+}
+
+RecursionNumOfOneToN(12);
+Console.WriteLine();
