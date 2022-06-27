c22f8bcba323c0323a307cadc60ffd74260ff269
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-06-27 21:49:28 +0500

    Task 2: Create new Array [,,] and print to console

--- Example034_HomeworkSeminar_10/Program.cs
+++ Example034_HomeworkSeminar_10/Program.cs
@@ -15,5 +15,29 @@ void RecursionNumOfOneToN(int count, int num = 1, int i = 0)
     RecursionNumOfOneToN(count - 1, num, i - 1);
 }
 
-RecursionNumOfOneToN(12);
-Console.WriteLine();
+//RecursionNumOfOneToN(12);
+//Console.WriteLine();
+
+
+//Task 2: Повторение матриц. Сформируйте трехмерный массив из неповторяющихся двухзначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+void CreateNewArrayAndPrint(int hight, int row, int column)
+{
+    int[,,] arr = new int[hight, row, column];
+    int startNumber = 10;
+
+    for (int i = 0; i < hight; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            for (int z = 0; z < column; z++)
+            {
+                arr[i, j, z] = startNumber++;
+                Console.WriteLine($"({i},{j},{z}) => {arr[i, j, z]}");
+            }
+        }
+    }
+}
+
+CreateNewArrayAndPrint(3, 4, 5);
